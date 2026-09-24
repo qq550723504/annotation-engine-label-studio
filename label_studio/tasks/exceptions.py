16283024483dbd788e3c0ff42a443b43de70304d
wei xu <xuweixia@live.com>
2026-09-24 10:52:40 +0800

    feat(assignments): add assignment concurrency conflict error

--- label_studio/tasks/exceptions.py
+++ label_studio/tasks/exceptions.py
@@ -5,3 +5,8 @@ from rest_framework import status
 class AnnotationDuplicateError(LabelStudioAPIException):
     status_code = status.HTTP_409_CONFLICT
     default_detail = 'Annotation with this unique id already exists'
+
+
+class AssignmentConflictError(LabelStudioAPIException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = 'Task assignment changed. Reload the task before saving.'
