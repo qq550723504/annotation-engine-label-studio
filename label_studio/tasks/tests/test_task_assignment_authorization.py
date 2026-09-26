99e6296b3f0d524642dd6f74dce630353ffbbb48
wei xu <xuweixia@live.com>
2026-09-26 18:30:53 +0800

    test(auth): reject foreign storage project IDs before validation

--- label_studio/tasks/tests/test_task_assignment_authorization.py
+++ label_studio/tasks/tests/test_task_assignment_authorization.py
@@ -109,6 +109,43 @@ class TestTaskAssignmentAuthorization(APITestCase):
             import_validate.assert_not_called()
             export_validate.assert_not_called()
 
+    def test_storage_create_foreign_project_id_is_denied_before_connection_validation(self):
+        other_creator = UserFactory(active_organization=self.organization)
+        self.organization.add_user(other_creator)
+        other_project = ProjectFactory(
+            organization=self.organization,
+            created_by=other_creator,
+        )
+
+        self.client.force_authenticate(user=self.annotator_a)
+        with (
+            patch('io_storages.s3.models.S3ImportStorage.validate_connection') as import_validate,
+            patch('io_storages.s3.models.S3ExportStorage.validate_connection') as export_validate,
+        ):
+            import_response = self.client.post(
+                '/api/storages/s3/',
+                data={
+                    'project': other_project.id,
+                    'bucket': 'foreign-project-import',
+                    'title': 'foreign-project-import',
+                },
+                format='json',
+            )
+            export_response = self.client.post(
+                '/api/storages/export/s3',
+                data={
+                    'project': other_project.id,
+                    'bucket': 'foreign-project-export',
+                    'title': 'foreign-project-export',
+                },
+                format='json',
+            )
+
+        assert import_response.status_code == 404
+        assert export_response.status_code == 404
+        import_validate.assert_not_called()
+        export_validate.assert_not_called()
+
     def test_assignee_can_open_assigned_task_but_not_someone_elses_task(self):
         self.client.force_authenticate(user=self.annotator_a)
 
